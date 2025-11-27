817ed962f64ad5bf11f51febcf758a57e5797766
Knoph Ayieko <knopholuoch@yahoo.com>
2025-11-27 10:22:14 +0300

    app.py 2

--- app.py
+++ app.py
@@ -7,6 +7,18 @@ from flask_caching import Cache
 app = Flask(__name__)
 cache = Cache(app, config={"CACHE_TYPE": "SimpleCache"})
 
+class BaseM3URetrievalException(Exception):
+    pass
+
+
+class M3ULinkNotFoundException(BaseM3URetrievalException):
+    pass
+
+
+class M3URetrievalHTTPException(BaseM3URetrievalException):
+    pass
+
+
 
 
 
